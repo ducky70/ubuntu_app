1f365c23a769f786489c9047103c5fe9643757d1
Yoon Lee <yoonlees@uiuc.edu>
2011-11-29 11:21:18 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UbutuApp::Application.config.secret_token = 'd9a57e564dc19b48d3aa8ed52cbb0a00e1c73cbd2cc502641d90f0d11ea2cddd959cedc8b9a620177a650a650846cb6ece4a106c8512e3ae1af7b7a5382b8a1e'
